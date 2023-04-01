71da1b5772f8374a47a6b880fe1a3998a12eb025
mim2041 <suraiyamim338@gmail.com>
2023-04-02 00:08:21 +0600

    retrived product prom stored cart and set it to cart state

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -16,8 +16,22 @@ const Shop = () => {
 
     useEffect( () => {
         const storedCart = getShoppingCart();
-        console.log(storedCart)
-    } , [])
+        const savedCart = [];
+        // step-1: get id of the addedProduct
+        for(const id in storedCart){
+            // get product from products state by using id
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                // step-3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+            console.log(addedProduct);
+        }
+        // step-5: set the cart
+        setCart(savedCart);
+    } ,[products])
 
     const handleAddToCart = product => {
         const newCart = [...cart, product]
